4b0d2734c5a65987eed53e79c2e2c842585c89d4
sielinou1605 <sielinou1605@gmail.com>
2026-07-15 08:17:28 +0100

    indicateur du mode de developpement

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // devIndicators: false, // Indicateurs du mode de développement désactivés
+/*  experimental: {
+    appDir: true, // Activation du répertoire d'application expérimentale
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignorer les erreurs de compilation TypeScript lors de la construction
+  },
+  */
 };
 
 export default nextConfig;
